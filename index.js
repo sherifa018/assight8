00f068ec96560760cbf3c831b269d1183bad83ca
sherifa <sherifaalsaiqe@gmail.com>
2024-10-29 11:42:38 -0500

    decond comment

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-let array = ["10", "20", "30", "40"];
-let newarr = array.map(function (x) {
-  return x * 2;
-});
-console.log(newarr);
+// //Task1
+// let array = ["10", "20", "30", "40"];
+// let newarr = array.map(function (x) {
+//   return x * 2;
+// });
+// console.log(newarr);
 
+//Task2
 let person = {
   firstname: "Sara",
   lastname: "saleh",
   age: 20,
 };
-for (var i in person) {
-  console.log(i + ":" + person[i]);
-}
 
-function display(arr2) {
-  arr2.forEach((str, index) => {
-    setTimeout(() => {
-      console.log(str);
-    }, index * 1000);
-  });
+function displayObject(obj) {
+  for (var key in obj) {
+    console.log(key + " : " + obj[key]);
+  }
 }
+displayObject(person);
 
-let arr2 = ["A", "B", "C", "D"];
-display(arr2);
+//Task3
+// function display(arr2) {
+//   arr2.forEach((str, index) => {
+//     setTimeout(() => {
+//       console.log(str);
+//     }, index * 1000);
+//   });
+// }
+
+// let arr2 = ["A", "B", "C", "D"];
+// display(arr2);
